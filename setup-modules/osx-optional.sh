2e1a2e05faeea082e6957f87b761fe309fd4ae59
Mark Fox <mf@ixel.org>
2016-06-02 23:35:26 -0700

    dealin with gamed

--- setup-modules/osx-optional.sh
+++ setup-modules/osx-optional.sh
@@ -11,3 +11,8 @@ echo 'Ensuring dark-mode enabled (toggle: control + option + command + t)'
 dark_mode=$(defaults read /Library/Preferences/.GlobalPreferences.plist _HIEnableThemeSwitchHotKey 2> /dev/null)
 [[ $dark_mode != 1 ]] \
    && sudo defaults write /Library/Preferences/.GlobalPreferences.plist _HIEnableThemeSwitchHotKey -bool true
+
+
+echo 'Ensuring gamed is unloaded'
+[[ -f /System/Library/LaunchAgents/com.apple.gamed.plist ]] \
+   && launchctl unload /System/Library/LaunchAgents/com.apple.gamed.plist 2> /dev/null
