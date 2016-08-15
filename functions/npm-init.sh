6bd50f222ec3eb04514597a30b150d5a3a5ba256
Mark Fox <mf@ixel.org>
2016-08-15 10:42:00 -0700

    npm_init: saves me the trouble of stubbing the same json file over and over

---
+++ functions/npm-init.sh
@@ -0,0 +1,9 @@
+
+npm_init() {
+   [[ -f package.json  ]] || echo "{\"name\": \"${PWD##*/}\", \"private\": true}" > package.json
+   [[ -f .editorconfig ]] || cp "$DOTFILES_DIR/configs/my-prefs.editorconfig" .editorconfig
+}
+
+npminit() {
+   npm_init
+}
