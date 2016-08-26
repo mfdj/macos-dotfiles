9ecdedc9a9133e3917b1e08d13a527e929db6daf
Mark Fox <mf@ixel.org>
2016-08-26 15:45:26 -0700

    atom-pacakges: added some recents

--- configs/atom-packages.sh
+++ configs/atom-packages.sh
@@ -4,11 +4,17 @@ require 'functions/apm-helpers'
 
 apm_ensure \
    atom-beautify \
+   color-picker \
+   docblockr \
    editorconfig \
    emmet \
    file-types \
    indent-guide-improved \
+   language-apache \
+   language-bats \
    language-gitignore \
+   language-htaccess \
+   language-json5 \
    linter \
    linter-shellcheck \
    pretty-json \
