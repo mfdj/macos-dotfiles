dc5c2f1a2c10314a36fe9bd5ebe3523c64a84829
Mark Fox <mf@ixel.org>
2016-08-26 15:23:12 -0700

    Because I'm a masochist I added a flag for root user (which requires hellish quoting)

--- bash-profile/dynamic/environment.sh
+++ bash-profile/dynamic/environment.sh
@@ -6,32 +6,57 @@ if brew --prefix git &> /dev/null; then
    echo source "${git_prefix}"/etc/bash_completion.d/git-completion.bash
 fi
 
-# None
+## None
 NO_COLOR='\e[0m'
-# Underlined
-UBLACK='\e[4;30m'
-# Bold (emphasized)
-EBLACK='\e[1;30m'
+
+## Underlined
+#UBLACK='\e[4;30m'
+
+## Bold (emphasized)
+#EBLACK='\e[1;30m'
 ERED='\e[1;31m'
-EWHITE='\e[1;37m'
-# Normal
+#EWHITE='\e[1;37m'
+
+## Normal
 LGREY='\e[0;37m'
-BLUE='\e[0;34m'
+#BLUE='\e[0;34m'
 CYAN='\e[0;36m'
 YELLOW='\e[0;33m'
 MAGENTA='\e[0;35m'
-RED='\e[0;31m'
-BLACK='\e[0;30m'
-WHITE='\e[0;37m'
-# Background
-BWHITE='\e[47m'
+#RED='\e[0;31m'
+#BLACK='\e[0;30m'
+#WHITE='\e[0;37m'
 
-# vcprompt -f '%b'    70ms
-# git_current_branch  110ms
+## Background
+#BWHITE='\e[47m'
+
+## NOTE: pre-compiling colors which probably isn't saving much CPU-time but demonstrates
+## robust mix of pre-compilied values and later-to-be interpreted syntax.
+
+echo "USER_COLOR='$YELLOW'
+if [[ \$UID == '0' || \$USER == 'root' ]]; then
+   USER_COLOR='$ERED'
+fi"
+
 if command -v vcprompt > /dev/null; then
-   echo "PS1='\n\[$LGREY\]\h \[$YELLOW\]\u\[$NO_COLOR\] \[$MAGENTA\]\w \[$CYAN\]\$(vcprompt -f '%b')\[$NO_COLOR\] \n→ '"
+   echo "PS1='\n\[$LGREY\]\h \['"'"'\$USER_COLOR'"'"'\]\u\[$NO_COLOR\] \[$MAGENTA\]\w \[$CYAN\]\$(vcprompt -f '%b')\[$NO_COLOR\] \n→ '"
+
 elif command -v git_current_branch > /dev/null; then
-   echo "PS1='\n\[$LGREY\]\h \[$YELLOW\]\u\[$NO_COLOR\] \[$MAGENTA\]\w \[$CYAN\]\$(git_current_branch)\[$NO_COLOR\] \n→ '"
+   echo "PS1='\n\[$LGREY\]\h \['"'"'\$USER_COLOR'"'"'\]\u\[$NO_COLOR\] \[$MAGENTA\]\w \[$CYAN\]\$(git_current_branch)\[$NO_COLOR\] \n→ '"
+
 else
-   echo "PS1='\n\[$LGREY\]\h \[$YELLOW\]\u\[$NO_COLOR\] \[$MAGENTA\]\w\[$NO_COLOR\] \n→ '"
+   echo 'PS1="'"\n\[$LGREY\]\h \[\${USER_COLOR}\]\u\[$NO_COLOR\] \[$MAGENTA\]\w\[$NO_COLOR\] \n→ "'"'
 fi
+
+## time vcprompt -f '%b'   --> 70ms
+## time git_current_branch --> 110ms
+#
+## sample output:
+#
+#    USER_COLOR='\e[0;33m'
+#    if [[ $UID == '0' || $USER == 'root' ]]; then
+#      USER_COLOR='\e[1;31m'
+#    fi
+#
+#    PS1='\n\[\e[0;37m\]\h \['"$USER_COLOR"'\]\u\[\e[0m\] \[\e[0;35m\]\w \[\e[0;36m\]$(vcprompt -f '%b')\[\e[0m\] \n→ '
+#
