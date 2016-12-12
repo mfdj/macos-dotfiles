9a7d8cb595b1474db1dcdeefcfcc11d84a598377
Mark Fox <mf@ixel.org>
2016-12-11 20:54:17 -0800

    search_itunes: switch to ripggrep as provider

--- functions/search-itunes.sh
+++ functions/search-itunes.sh
@@ -1,7 +1,7 @@
 # shellcheck disable=SC2148
 
 search_itunes() {
-   grep -i "$*" ~/Music/iTunes/iTunes\ Music\ Library.xml -A2
+   rg -i "$*" ~/Music/iTunes/iTunes\ Music\ Library.xml -A2
 }
 
 alias itunes_search='search_itunes'
