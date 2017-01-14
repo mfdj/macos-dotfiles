dc766f8c6c293292441d3643aad29f82c9a1b3fc
Mark Fox <mf@ixel.org>
2017-01-13 23:39:09 -0800

    started functions/php-helpers: xdebugon and xdebugoff

---
+++ functions/php-helpers.sh
@@ -0,0 +1,22 @@
+# shellcheck disable=SC2148
+
+xdebugenable() {
+   local phpfpm=$(brew services list | grep started | grep php | awk '{print $1}')
+   local ini=$(php -i | grep xdebug.ini)
+   local pattern='s~^zend_extension~;zend_extension~'
+   [[ $1 ]] && pattern='s~^;zend_extension~zend_extension~'
+
+   # BSD sed -i behavior
+   sed -i .orig "$pattern" "$ini"
+   rm "$ini.orig"
+
+   [[ $phpfpm ]] && brew services restart "$phpfpm"
+}
+
+xdebugon() {
+   xdebugenable
+}
+
+xdebugoff() {
+   xdebugenable $(false)
+}
