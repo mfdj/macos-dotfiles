f876c49ae9dcb9be7d2502419865aa099d698351
Mark Fox <mf@ixel.org>
2017-01-19 23:04:02 -0800

    fix xdebugon/xdebugoff

--- functions/php-helpers.sh
+++ functions/php-helpers.sh
@@ -3,8 +3,8 @@
 xdebugenable() {
    local phpfpm=$(brew services list | grep started | grep php | awk '{print $1}')
    local ini=$(php -i | grep xdebug.ini)
-   local pattern='s~^zend_extension~;zend_extension~'
-   [[ $1 ]] && pattern='s~^;zend_extension~zend_extension~'
+   local pattern='s~^;zend_extension~zend_extension~'
+   [[ $1 == false ]] && pattern='s~^zend_extension~;zend_extension~'
 
    # BSD sed -i behavior
    sed -i .orig "$pattern" "$ini"
@@ -18,5 +18,5 @@ xdebugon() {
 }
 
 xdebugoff() {
-   xdebugenable $(false)
+   xdebugenable false
 }
