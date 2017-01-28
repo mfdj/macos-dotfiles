f80538b8368110088e7ce5ce0a0a61655893e455
Mark Fox <mf@ixel.org>
2017-01-28 12:47:03 -0800

    apm_enusre: refactor to not use ls

--- functions/apm-helpers.sh
+++ functions/apm-helpers.sh
@@ -2,8 +2,8 @@
 
 apm_ensure() {
    for package in "$@"; do
-      ls -l ~/.atom/packages | grep "$package" -q ||
-         apm list | grep $package -q ||
-         apm install $package
+      (cd ~/.atom/packages; find . -type d -maxdepth 1 | grep -q "$package") ||
+         apm list | grep "$package" -q ||
+         apm install "$package"
    done
 }
