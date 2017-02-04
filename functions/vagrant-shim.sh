d34645be9ad7879f989dba192cc8a2b2f854f668
Mark Fox <mf@ixel.org>
2017-02-04 13:19:05 -0800

    vagrant-shim: started ssh-shim, namely update_ssh_config

--- functions/vagrant-shim.sh
+++ functions/vagrant-shim.sh
@@ -11,47 +11,58 @@ vagrant() {
    local project_hash
    local ssh_config_cache
 
-   if [[ -f Vagrantfile ]]; then
-      has_vagrantfile=true
-      project_path=$(pwd)
-      project_hash=$(md5 <<< "$(pwd)")
-      ssh_config_cache=/tmp/vagrant-ssh-config-${project_hash}
-   fi
-
    pipe_safe_color() {
       if [ -t 1 -a -t 0 ]; then
          # not in-a-pipe or file-redireciton
          if [[ $1 =~ (red|green|bold|yellow) ]]; then
-            [[ $1 == red    ]] && echo -e "\033[1;91mvagrant-shim: ${@:2}\033[0m"
-            [[ $1 == green  ]] && echo -e "\033[1;32mvagrant-shim: ${@:2}\033[0m"
-            [[ $1 == yellow ]] && echo -e "\033[1;33mvagrant-shim: ${@:2}\033[0m"
-            [[ $1 == bold   ]] && echo -e "\033[1;1mvagrant-shim: ${@:2}\033[0m"
+            [[ $1 == red    ]] && echo -e "\033[1;91m${@:2}\033[0m"
+            [[ $1 == green  ]] && echo -e "\033[1;32m${@:2}\033[0m"
+            [[ $1 == yellow ]] && echo -e "\033[1;33m${@:2}\033[0m"
+            [[ $1 == bold   ]] && echo -e "\033[1;1m${@:2}\033[0m"
             return
          fi
       fi
 
-      echo "vagrant-shim: ${@}"
+      echo "${@}"
    }
 
    log() {
       local color
 
       if [[ $1 == debug ]]; then
-         [[ $DEBUG ]] && pipe_safe_color "${@:2}"
+         [[ $DEBUG ]] && pipe_safe_color "vagrant-shim: ${@:2}"
       else
          color=bold # info is default
          [[ $1 == error   ]] && color=red
          [[ $1 == success ]] && color=green
-         [[ $1 == warning ]] && color=yellow
-         [[ $1 =~ (error|success|info|warning) ]] && shift
+         [[ $1 == warn    ]] && color=yellow
+         [[ $1 =~ (error|success|info|warn) ]] && shift
 
-         pipe_safe_color "$color" "${@}"
+         if [[ $DEBUG ]]; then
+            pipe_safe_color "$color" "vagrant-shim: ${@}"
+         else
+            pipe_safe_color "$color" "${@}"
+         fi
       fi
    }
 
+   if [[ -f Vagrantfile ]]; then
+      has_vagrantfile=true
+      project_path=$(pwd)
+      project_hash=$(md5 <<< "$(pwd)")
+      ssh_config_cache=/tmp/vagrant-ssh-config-${project_hash}
+
+      log debug "has_vagrantfile  '$has_vagrantfile'"
+      log debug "project_path     '$project_path'"
+      log debug "project_hash     '$project_hash'"
+      log debug "ssh_config_cache '$ssh_config_cache'"
+   fi
+
+   # ~=~=~=~ status-shim ~=~=~=~
+
    if [[ $1 == status ]]; then
       if [[ -z $has_vagrantfile ]]; then
-         log warning 'Vagrantfile missing, showing global-status'
+         log warn 'Vagrantfile missing, showing global-status'
          command vagrant global-status
 
          # preserves behavior for scripts that use exit code of `vagrant status`
@@ -62,6 +73,41 @@ vagrant() {
       command vagrant status
       return $?
    fi
+
+   # ~=~=~=~ ssh-shim ~=~=~=~
+
+   update_ssh_config() {
+      if [[ -z $1 ]]; then
+         log warn 'update_ssh_config missing cache-path'
+         return 1
+      else
+         log 'updating cached ssh-config'
+         command vagrant ssh-config > "$1" || return 1
+      fi
+
+      if grep -q 'User vagrant' "$1" 2> /dev/null; then
+         log debug "'$1' looks like valid ssh-config"
+      else
+         log debug "'$1' does not look like valid ssh-config"
+         return 1
+      fi
+   }
+
+   if [[ $1 == ssh && $has_vagrantfile ]]; then
+      [[ -s "$ssh_config_cache" ]] || update_ssh_config "$ssh_config_cache"
+
+      update_ssh_config '/tmp/asdf' || return 1
+
+      if [[ -s "$ssh_config_cache" ]]; then
+         log debug "trying ssh with '$ssh_config_cache'"
+
+         return
+      else
+         log error "skipping ssh-shim because ssh_config_cache is empty"
+      fi
+   fi
+
+   log debug "passing '$@' to vagrant"
 
    command vagrant "$@"
 }
