29800eddabed507e9a9318eb123dc61703474f57
Mark Fox <mf@ixel.org>
2017-02-04 20:38:09 -0800

    vagrant-shim: rename log to vglog; added ssh-config and ssh-config-file

--- functions/vagrant-shim.sh
+++ functions/vagrant-shim.sh
@@ -6,7 +6,7 @@ vagrant() {
    local box_name
    local project_path
    local project_hash
-   local ssh_config_cache
+   local ssh_config_path
    local vagrant_option_found
    local active_hosts
    local selected_host
@@ -24,10 +24,10 @@ vagrant() {
          fi
       fi
 
-      echo "${@}"
+      (&>2 echo "${@}")
    }
 
-   log() {
+   vglog() {
       local color
 
       if [[ $1 == debug ]]; then
@@ -55,21 +55,21 @@ vagrant() {
       has_vagrantfile=true
       project_path=$(pwd)
       project_hash=$(md5 <<< "$(pwd)")
-      ssh_config_cache=/tmp/vagrant-ssh-config-${project_hash}
+      ssh_config_path=/tmp/vagrant-ssh-config-${project_hash}
 
-      log debug "has_vagrantfile  '$has_vagrantfile'"
-      log debug "project_path     '$project_path'"
-      log debug "project_hash     '$project_hash'"
-      log debug "ssh_config_cache '$ssh_config_cache'"
+      vglog debug "has_vagrantfile  '$has_vagrantfile'"
+      vglog debug "project_path     '$project_path'"
+      vglog debug "project_hash     '$project_hash'"
+      vglog debug "ssh_config_path '$ssh_config_path'"
    fi
 
    # ~=~=~=~ status-shim ~=~=~=~
 
    if [[ $1 == status ]]; then
-      log debug 'shimming vagrant status'
+      vglog debug 'shimming vagrant status'
 
       if [[ -z $has_vagrantfile ]]; then
-         log warn 'Vagrantfile missing, showing global-status'
+         vglog warn 'Vagrantfile missing, showing global-status'
          command vagrant global-status
 
          # preserves behavior for scripts that use exit code of `vagrant status`
@@ -85,22 +85,28 @@ vagrant() {
 
    check_sssh_config() {
       if command grep -q 'User vagrant' "$1"; then
-         log debug "'$1' looks like valid ssh-config"
+         vglog debug "'$1' looks like valid ssh-config"
          return 0
       else
-         log warn "'$1' does not look like valid ssh-config"
+         vglog warn "'$1' does not look like valid ssh-config"
          return 1
       fi
    }
 
    update_ssh_config() {
       if [[ -z $1 ]]; then
-         log warn 'update_ssh_config missing cache-path'
+         vglog warn 'update_ssh_config missing cache-path'
          return 1
-      else
-         log 'updating cached ssh-config'
-         command vagrant ssh-config > "$1" || return 1
       fi
+
+      vglog 'updating cached ssh-config'
+      command vagrant ssh-config > "$1" 2> "$ssh_config_path-err"
+
+      vglog debug "vagrant ssh-config exited with: $?"
+
+      while read line; do
+         vglog debug "ssh-config err: $line"
+      done < "$ssh_config_path-err"
 
       check_sssh_config "$1"
    }
@@ -108,8 +114,8 @@ vagrant() {
    ssh_shim() {
       local sshexit
 
-      log debug "ssh_shim options - config: '$1' host: '$2' command: '$3'"
-      log "ssh-shim running '$3' on '$2'"
+      vglog debug "ssh_shim options - config: '$1' host: '$2' command: '$3'"
+      vglog "ssh-shim running '$3' on '$2'"
 
       ssh -F "$1" "$2" "$3"
       # \
@@ -117,69 +123,85 @@ vagrant() {
       #    2> /tmp/vagranth-ssh-${project_hash}-first-err
 
       sshexit=$?
-      log debug "ssh first exit-code: $sshexit"
+      vglog debug "ssh first exit-code: $sshexit"
 
       # cat /tmp/vagranth-ssh-${project_hash}-first-err
       # cat /tmp/vagranth-ssh-${project_hash}-first
 
       if (( $sshexit == 255 )); then
-         log 'ssh-shim retrying'
+         vglog 'ssh-shim retrying'
          update_ssh_config "$1" && {
-            log debug 'ssh-shim second attempt'
+            vglog debug 'ssh-shim second attempt'
             ssh -F "$1" "$2" "$3"
          }
       fi
    }
 
    if [[ $1 == ssh && $has_vagrantfile ]]; then
-      log debug 'shimming vagrant ssh'
+      vglog debug 'shimming vagrant ssh'
 
       vagrant_option_found=''
 
       for word in "${@:2}"; do
-         log debug "checking parameter '$word'"
+         vglog debug "checking parameter '$word'"
          [[ $word =~ ^- ]] && {
-            log debug "shipping-ssh-shim becuase of '$word'"
+            vglog debug "shipping-ssh-shim becuase of '$word'"
             vagrant_option_found=true
          }
       done
 
       if [[ ! $vagrant_option_found ]]; then
-         log debug 'no vagrant-flags found, proceeding with ssh-shim'
+         vglog debug 'no vagrant-flags found, proceeding with ssh-shim'
 
-         [[ -s $ssh_config_cache ]] || update_ssh_config "$ssh_config_cache"
+         [[ -s $ssh_config_path ]] || update_ssh_config "$ssh_config_path"
 
-         if check_sssh_config "$ssh_config_cache"; then
-            log debug "trying ssh with '$ssh_config_cache'"
+         if check_sssh_config "$ssh_config_path"; then
+            vglog debug "trying ssh with '$ssh_config_path'"
 
             # check host against active-hosts
-            active_hosts=$(grep -E '^Host .+' "$ssh_config_cache" | awk '{print $2}')
-            log debug "active-hosts [$(tr '\n' ',' <<< "$hosts" | sed 's/,$//')]"
+            active_hosts=$(grep -E '^Host .+' "$ssh_config_path" | awk '{print $2}')
+            vglog debug "active-hosts [$(tr '\n' ',' <<< "$hosts" | sed 's/,$//')]"
 
             # check if param-2 is active-host
             selected_host=$(grep -E "^${2}\$" <<< "$active_hosts")
 
             if [[ $selected_host ]]; then
-               log debug "using '$selected_host'"
+               vglog debug "using '$selected_host'"
                shift
             else
-               log debug "param-2: '$2' is not a valid host"
+               vglog debug "param-2: '$2' is not an active host"
 
                # select default
                selected_host=$(head -n1 <<< "$active_hosts")
-               log debug "using default '$selected_host'"
+               vglog debug "using first active host '$selected_host'"
             fi
 
-            ssh_shim "$ssh_config_cache" "$selected_host" "$2"
+            ssh_shim "$ssh_config_path" "$selected_host" "$2"
             return $?
          else
-            log error "skipping ssh-shim because ssh_config_cache is empty"
+            vglog error "stopping ssh-shim because ssh-config is invalid"
             return 1
          fi
       fi
    fi
 
-   log debug "passing '$@' to vagrant"
+   if [[ $1 == ssh-config && $has_vagrantfile ]]; then
+      vglog debug "ssh-config refreshing $ssh_config_path"
+
+      if update_ssh_config "$ssh_config_path"; then
+         cat "$ssh_config_path"
+         return 0
+      fi
+      return 1
+   fi
+
+   if [[ $1 == ssh-config-file && $has_vagrantfile ]]; then
+      vglog debug "ssh-config refreshing $ssh_config_path"
+      echo "$ssh_config_path"
+      return 0
+   fi
+
+   vglog debug "passing '$@' to vagrant"
 
    command vagrant "$@"
 }
