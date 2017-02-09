79a82edffbf318460c1ab2f1001db4a6a8b66f3b
Mark Fox <mf@ixel.org>
2017-02-09 13:36:32 -0800

    apm_ensure: fixed case where no packages are installed

--- functions/apm-helpers.sh
+++ functions/apm-helpers.sh
@@ -2,7 +2,7 @@
 
 apm_ensure() {
    for package in "$@"; do
-      (cd ~/.atom/packages; find . -type d -maxdepth 1 | grep -q "$package") ||
+      (cd ~/.atom/packages 2> /dev/null && find . -type d -maxdepth 1 | grep -q "$package") ||
          apm list | grep "$package" -q ||
          apm install "$package"
    done
