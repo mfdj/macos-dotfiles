9690f305540f9fc2abfd001c14560393dad9214e
Mark Fox <mf@ixel.org>
2017-02-25 13:09:59 -0800

    curl-helpers: added curlgrep

--- functions/curl-helpers.sh
+++ functions/curl-helpers.sh
@@ -12,3 +12,26 @@ curlpjson() {
       --header 'Accept: application/json' --header 'Content-Type: application/json' \
       --data "$2" | grep '}' | python -mjson.tool
 }
+
+# preference precedence: ripgrep, egrep, grep
+curlgrep() {
+   local result
+   result=/tmp/curl-grep-result
+
+   if [[ $3 == last ]]; then
+      echo "reusing $result-$1"
+   else
+      curl "$1" > "$result-$1"
+      echo # clear last-line of curl's stderr
+   fi
+
+   if command -v rg &> /dev/null; then
+      rg "$2" "$result-$1"
+
+   elif command -v egrep &> /dev/null; then
+      egrep "$2" "$result-$1"
+
+   else
+      grep "$2" "$result-$1"
+   fi
+}
