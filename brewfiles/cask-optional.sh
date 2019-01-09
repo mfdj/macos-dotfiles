e7ee6c4941f473e9ed11c52cc3c07a83334ca53d
Mark Fox <mf@ixel.org>
2019-01-09 09:44:54 -0800

    brewfiles add: kap, sublime-text, visual-studio-code

--- brewfiles/cask-optional.sh
+++ brewfiles/cask-optional.sh
@@ -5,6 +5,7 @@ require 'functions/brew-helpers'
 cask_ensure  charles
 cask_ensure  chefdk
 cask_ensure  firefox
+cask_ensure  kap
 cask_ensure  licecap
 cask_ensure  lighttable
 cask_ensure  murus
@@ -12,8 +13,10 @@ cask_ensure  openoffice
 cask_ensure  rubymine
 cask_ensure  processing
 cask_ensure  screenflow
+cask_ensure  sublime-text
 cask_ensure  shiftit # alternative to spectacle
 cask_ensure  sketch
 cask_ensure  vagrant
 cask_ensure  virtualbox
+cask_ensure  visual-studio-code
 cask_ensure  vlc
