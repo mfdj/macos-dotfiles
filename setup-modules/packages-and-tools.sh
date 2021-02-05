4676dddcc0f17e5ff4a5d3def6ab139cb7ec666c
Mark Fox <mf@ixel.org>
2021-02-05 13:27:25 -0800

    switch hombrew installer from ruby to shell scrit

--- setup-modules/packages-and-tools.sh
+++ setup-modules/packages-and-tools.sh
@@ -23,7 +23,7 @@ fi
 
 echo Ensuring Homebrew
 [[ ! -f /usr/local/bin/brew ]] &&
-   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # ! command -v brew > /dev/null && {
 #    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
