7556444d80cf1761ee4b9c6483a5c2bd9fdb3335
Mark Fox <mf@ixel.org>
2021-04-27 09:34:34 -0700

    shim bundler so it defaults to --local

--- functions/almost-aliases.sh
+++ functions/almost-aliases.sh
@@ -17,6 +17,17 @@ der2pem() {
    openssl x509 -inform der -outform pem -in $1 -out $1.pem
 }
 
+bundle() {
+   if (( $# > 0 )); then
+      command bundle "$@"
+   else
+      { >&2 echo "adding --local"; }
+      # https://bundler.io/v2.2/man/bundle-install.1.html
+      # --local will attempt to skip rubygems.org and use Rubygems' cache or in vendor/cache
+      command bundle install --local || bundle install
+   fi
+}
+
 vagrant() {
    vagrant-shim "$@"
 }
