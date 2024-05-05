606a0d2b2fc2365d84de79e145eee98098f73a29
Mark Fox <mf@ixel.org>
2024-05-05 00:26:07 -0700

    Improve install.sh process
     * streamline ssh key generation output and flesh out code comments
     * allow installation path to be configurbale with DOTFILES_DIR
     * try to detect more problems
     * run setup as part of install

--- functions/ssh-helpers.sh
+++ functions/ssh-helpers.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
 
+#
+# Will display or generate an an SSH key in the preferred format.
+#  • will interactively prompts for a passphrase during generation
+#
 ensure_ssh_key() {
-   echo Ensuring appropriate SSH key exists
-   if [[ ! -f ~/.ssh/id_ed25519 ]]; then
+   if [[ -f ~/.ssh/id_ed25519 ]]; then
+      echo Found SSH key ~/.ssh/id_ed25519
+      ssh-keygen -l -f ~/.ssh/id_ed25519
+   else
       echo 'Generating new ssh key (ed25519)'
-      # NOTE: the default file path is ~/.ssh/id_${algo}
-      # NOTE: will interactively prompt for a passphrase.
+      # • pass -f so it doesn't prompt for output_keyfile; conventional default is ~/.ssh/id_${algo}
+      # • don't pass -C since the default comment "$(whoami)@$(scutil --get ComputerName)" is good
       ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519
+   fi
+
+   # if key is encrypted this command will exit with 255 because -P '' passes an empty passphrase
+   if ! ssh-keygen -y -P '' -f ~/.ssh/id_ed25519 &> /dev/null; then
+      echo 'Adding encrypted key to keychain'
+      # prior to macOS 12 you would ssh-add -K …
+      ssh-add --apple-use-keychain ~/.ssh/id_ed25519
    fi
 }
--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-if [[ ${DOTFILES_DIR:-} ]] && [[ -f "$DOTFILES_DIR/setup-modules/_env.bash" ]]; then
+# NOTE: setup-modules/_env.bash will also set DOTFILES_DIR (if it's unset) so we
+# use INSTALL_INTO to differneatie
+INSTALL_INTO=${DOTFILES_DIR:-$HOME/dotfiles}
+echo "Installing dotfiles into $INSTALL_INTO"
+
+if [[ -f "$INSTALL_INTO/setup-modules/_env.bash" ]]; then
    # shellcheck source=./setup-modules/_env.bash
-   source "$DOTFILES_DIR/setup-modules/_env.bash"
+   source "$INSTALL_INTO/setup-modules/_env.bash"
 elif [[ -f setup-modules/_env.bash ]]; then
    source setup-modules/_env.bash
 else
@@ -19,18 +24,45 @@ sourcedot brewfiles/essential
 
 ensure_ssh_key
 
-echo "Ensuring dotfiles are cloned to $HOME/dotfiles"
-if [[ -d ~/dotfiles ]]; then
+if [[ -d $INSTALL_INTO ]]; then
    (
-      cd ~/dotfiles
-      git status || {
-         echo "Error: $HOME/dotfiles was not git cloned"
-      }
+      cd "$INSTALL_INTO"
+      if git status --short --branch; then
+         echo Valid repository
+
+         if git remote -v | grep -q https; then
+            echo "Error: $INSTALL_INTO was cloned over https - git protocol is required to receive updates"
+            # TODO: add option to backup and clean current INSTALL_INTO directory so installer can continue
+            exit 1
+         fi
+
+         # TODO: make expected git origin configurable
+         if ! git remote -v | grep -q 'mfdj/macos-dotfiles'; then
+            echo "Error: $INSTALL_INTO was not cloned from expected origin (mfdj/macos-dotfiles)"
+            exit 1
+         fi
+
+         if ! git fetch; then
+            echo Ensure Github has the public key
+            cat ~/.ssh/id_ed25519.pub
+            echo 'open https://github.com/settings/keys'
+            exit 1
+         fi
+      else
+         echo "Error: $INSTALL_INTO was not git cloned"
+         exit 1
+      fi
    )
 else
-   if ! git clone git@github.com:mfdj/macos-dotfiles.git ~/dotfiles; then
-      echo "Ensure Github has the public key"
+   if ! git clone git@github.com:mfdj/macos-dotfiles.git "$INSTALL_INTO"; then
+      echo Ensure Github has the public key
       cat ~/.ssh/id_ed25519.pub
       echo 'open https://github.com/settings/keys'
+      exit 1
    fi
 fi
+
+# Ready to setup
+export DOTFILES_DIR=${INSTALL_INTO}
+cd "$DOTFILES_DIR"
+./setup.sh
