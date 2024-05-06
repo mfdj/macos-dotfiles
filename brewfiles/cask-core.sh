1b4b72f2276be74bcc10d25301823b378b9fdb2e
Mark Fox <mf@ixel.org>
2024-05-06 16:33:42 -0700

    Add kaleidoscope v3 installer

--- brewfiles/cask-core.sh
+++ brewfiles/cask-core.sh
@@ -6,10 +6,20 @@ echo -n Ensuring cask-core packages
 
 cask_ensure  app-tamer
 cask_ensure  flux
-cask_ensure  kaleidoscope
+# mainline kaleidoscope is version 4; see version 3 installer
+#cask_ensure  kaleidoscope
 cask_ensure  macdown
 cask_ensure  sourcetree
 cask_ensure  sequel-pro
+
+# kaleidoscope v3
+if which -s ksdiff; then
+   echo -n " ✔ kaleidoscope (v3)"
+else
+   curl https://raw.githubusercontent.com/Homebrew/homebrew-cask/d81b3ccdd74823f6c9ef5f1adbbd6e717e2b1e0a/Casks/kaleidoscope.rb > ~/Downloads/kaleidoscope.rb
+   brew install ~/Downloads/kaleidoscope.rb && echo -n " ✔ kaleidoscope (v3)"
+   rm ~/Downloads/kaleidoscope.rb || true
+fi
 
 # defer to pre-installed software
 cask_ensure_unless_directory google-chrome  /Applications/Google\ Chrome.app # at one point installing outside of cask fixed an issue with 1password extension
